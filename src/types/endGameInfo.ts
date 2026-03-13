efe45a7d60894e4089b0dae47b34fc9eb8a8c25d
ig-ealbert <elalbert42@gmail.com>
2026-03-13 17:41:01 -0400

    Custom types are no longer necessary

--- src/types/endGameInfo.ts
+++
@@ -1,5 +0,0 @@
-export type endGameInfo = {
-  userGuess: string;
-  secretWord: string;
-  remainingGuesses: number;
-};
--- src/types/letterStyleInfo.ts
+++
@@ -1,7 +0,0 @@
-export type letterStyleInfo = {
-  guess: string;
-  letterIndex: number;
-  attempt: number;
-  secretWord: string;
-  yellowIndices: number[][];
-};
